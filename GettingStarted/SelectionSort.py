3e3f40f065b87e0b081693f5ae3a1a8738b5d90d
Vidit Shah <vidit0210@gmail.com>
2020-09-12 01:16:21 +0530

    a1

--- GettingStarted/SelectionSort.py
+++ GettingStarted/SelectionSort.py
@@ -1,9 +1,11 @@
 def selection_sort(A):
+    minimium = None
     for i in range(len(A)):
         minimum = A[i]
-        for j in range(len(A)):
+        for j in range(i + 1, len(A)):
             if A[j] < minimum:
-                A[i] = A[j]
+                minimium = j
+    A[i], A[minimium] = A[minimium], A[i]
 
 
 A = [3, 7, 1, 2, 4, 5]
