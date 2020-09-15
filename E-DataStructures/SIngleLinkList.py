c110b9b548cb0b51832e23442b8daa0d04491f26
Vidit Shah <vidit0210@gmail.com>
2020-09-15 19:12:47 +0530

    Node

--- E-DataStructures/SIngleLinkList.py
+++ E-DataStructures/SIngleLinkList.py
@@ -1,40 +1,42 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next_element = None
+from Node import Node
 
 
 class LinkedList:
     def __init__(self):
         self.head_node = None
 
-    def get_head(self):
-        return self.head_node
+    # Insertion at Head
+    def insert_at_head(self, data):
+        # Create a new node containing your specified value
+        temp_node = Node(data)
+        # The new node points to the same node as the head
+        temp_node.next_element = self.head_node
+        self.head_node = temp_node  # Make the head point to the new node
+        return self.head_node  # return the new list
 
     def is_empty(self):
-        return True if self.head_node is None else False
-
-    def insert_at_head(self, data):
-        temp_node = Node(data)
-        temp_node.next_element = self.head_node
-        self.head_node = temp_node
-        return self.head_node
-
-    def print_list(self):
-        if (self.is_empty()):
-            print("list is empty")
+        if self.head_node is None:
+            return True
+        else:
             return False
-        temp_node = self.head_node
-        while self.head_node is not None:
-            print(temp_node.data, end="->")
-            temp_node = temp_node.next_element
-        print(temp_node.data, "-> None")
+
+# Supplementary print function
+    def print_list(self):
+        if(self.is_empty()):
+            print("List is Empty")
+            return False
+        temp = self.head_node
+        while temp.next_element is not None:
+            print(temp.data, end=" -> ")
+            temp = temp.next_element
+        print(temp.data, "-> None")
         return True
 
 
-singleLinkList = LinkedList()
-singleLinkList.print_list()
-print("Inserting Values in the list")
+list = LinkedList()
+list.print_list()
+
+print("Inserting values in list")
 for i in range(1, 10):
-    singleLinkList.insert_at_head(i)
-singleLinkList.print_list()
+    list.insert_at_head(i)
+list.print_list()
