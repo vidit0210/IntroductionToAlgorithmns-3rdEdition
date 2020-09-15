97f6ce1569c077f28abf529948f2a8cb2510714b
Vidit Shah <vidit0210@gmail.com>
2020-09-15 18:59:25 +0530

    insertAthead

--- E-DataStructures/SIngleLinkList.py
+++ E-DataStructures/SIngleLinkList.py
@@ -1,4 +1,21 @@
 class Node:
     def __init__(self, data):
         self.data = data
-        self.pointer = None
+        self.next_element = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head_node = None
+
+    def get_head(self):
+        return self.head_node
+
+    def isEmpty(self):
+        return True if self.head_node is None else False
+
+    def insert_at_head(self, data):
+        temp_node = Node(data)
+        temp_node.next_element = self.head_node
+        self.head_node = temp_node
+        return self.head_node
